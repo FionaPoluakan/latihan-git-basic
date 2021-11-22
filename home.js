063588adec6636c2fbba40840a2985cecafb3786
FionaPoluakan <85221799+FionaPoluakan@users.noreply.github.com>
2021-11-22 20:17:05 +0800

    tambah baris kedelapan

--- home.js
+++ home.js
@@ -6,4 +6,10 @@ console.log("Baris kedua");
 
 console.log("Baris ketiga");
 console.log("Baris keempat dari online");
+console.log("Baris kelima");
+console.log("Baris keenam");
+console.log("Baris ketujuh");
+
+//Jumat
+console.log("Baris kedelapan");
 
