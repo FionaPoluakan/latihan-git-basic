ebee0c443ea66c6494c3ed813ffbc53c2a7a49a1
fionapoluakan <poluakanfiona@gmail.com>
2021-11-22 10:47:42 +0800

    tambah baris ketujuh file index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 //Senin
 console.log("Baris pertama");
 
-
 //Selasa
 console.log("Baris kedua");
 console.log("Baris ketiga");
 
-
 //Rabu
 console.log("Baris keempat");
+console.log("Baris kelima");
+
+//Kamis
+console.log("Baris keenam");
+
+//Jumat
+console.log("Baris ketujuh");
